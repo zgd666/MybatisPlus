65a381330bba92a28c5f4aea66d1c4d5f7cf2073
zhangguoding <m18111007369@163.com>
2023-03-20 22:49:32 +0800

    Initial commit

---
+++ src/test/java/com/zgd/MybatisPlusApplicationTests.java
@@ -0,0 +1,27 @@
+package com.zgd;
+
+import com.zgd.mapper.UserMapper;
+import com.zgd.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class MybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        System.out.println(userList.size());
+    }
+
+}
